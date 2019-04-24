3d8b1378eada4c8edd4f7bd7fc267624ebc92efc
Kihoon <skfk3606@naver.com>
2019-04-24 18:15:10 +0900

    update scripts

--- auturbo_2019_spring/auturbo_2019_spring_week6/carla_opencv/src/3houghLine.py
+++ auturbo_2019_spring/auturbo_2019_spring_week6/carla_opencv/src/3houghLine.py
@@ -14,21 +14,33 @@ class Hough():
         self.bridge = CvBridge()
         
     def callback(self, image_msg):
-        cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+    	src = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+    	dst = cv2.Canny(src, 50, 200, None, 3)
+    	cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    	cdstP = np.copy(cdst)
+    	lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
 
-        gray = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray,50,150,apertureSize = 3)
-        minLineLength = 100
-        maxLineGap = 10
-        lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    	if lines is not None:
+    	    for i in range(0, len(lines)):
+	        rho = lines[i][0][0]
+	        theta = lines[i][0][1]
+	        a = math.cos(theta)
+	        b = math.sin(theta)
+	        x0 = a * rho
+	        y0 = b * rho
+	        pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+	        pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+	        cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+	linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
 
-        for x1,y1,x2,y2 in lines[0]:
-            cv2.line(cv_image,(x1,y1),(x2,y2),(0,255,0),2)
-
-
-
-        cv2.imshow('hough', cv_image), cv2.waitKey(1)
-        cv2.imshow('edge', edges), cv2.waitKey(1)
+	if linesP is not None:
+	    for i in range(0, len(linesP)):
+	        l = linesP[i][0]
+	        cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 1, cv2.LINE_AA)
+	 
+	cv2.imshow("Source", src), cv2.waitKey(1)
+	cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst), cv2.waitKey(1)
+	cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP), cv2.waitKey(1)
 
     def main(self):
         rospy.spin()
